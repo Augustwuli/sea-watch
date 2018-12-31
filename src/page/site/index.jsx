856b7665d5e4a7ad417a73de8da5a4fc156fe943
wxh_leo <754557634@qq.com>
2018-12-31 16:48:19 +0800

    渲染出一个页面

--- src/page/site/index.jsx
+++ src/page/site/index.jsx
@@ -1,19 +1,39 @@
 import React, {Component} from 'react'
+import { Link } from 'react-router-dom'
 import Api from '@/tool/api'
 
 export default class Index extends Component {
   constructor (props) {
     super(props)
-    this.state = {}
+    this.state = {
+      list: []
+    }
   }
   componentDidMount () {
+    this.getData()
+  }
+  getData () {
     Api.get('topics', null, r => {
-      console.log(r)
+      this.setState({list: r.data})
     })
   }
   render () {
+    let { list } = this.state
+    let dom = null
+    if (list.length !== 0) {
+      let listDom = list.map((i,k)=>{
+        return (
+          <li key={k}><Link to={`/details/${i.id}`}>{i.title}</Link></li>
+        )
+      })
+      dom = (
+        <div className="topic_list">
+          <ul>{listDom}</ul>
+        </div>
+      )
+    }
     return (
-      <div className="outer home">index Page</div>
+      <div className="outer home">{dom}</div>
     )
   }
 }
